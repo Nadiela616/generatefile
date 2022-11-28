4c62cdfd1b3ce8467ff8800a32fd48cf373451e3
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-11-28 17:16:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-import { clear } from 'console';
+import fsp from 'fs/promises';
 import fs from 'fs';
 
-function callback(error, content) {
-    if(error) {
-        console.log('The file does not exist!');
+async function parsedData(content) {    
+    const lines = content.split('\n');
+    const data = [];
+
+    for(let line of lines){
+        const tokens = line.split(',');
+            
+        const clear_data = {};
+        clear_data.filename = tokens[0];
+        clear_data.filecontent = tokens[1];
+        data.push(clear_data);            
     }
-    else {
-        const lines = content.split('\n');
-        const data = [];
 
-        for(let line of lines){
-            const tokens = line.split(',');
-            
-            const clear_data = {};
-            clear_data.filename = tokens[0];
-            clear_data.filecontent = tokens[1];
-
-            data.push(clear_data);
-            
-        }
-        for(let item of data) {
-
-            
-            fs.writeFile(item.filename, item.filecontent, (error) => {
-                    if (error)
-                        throw error;
-                    console.log('Saved!');
-                });
-
-
-        }
+    for(let item of data) {           
+        await fsp.writeFile(item.filename, item.filecontent);
     }
 }
 
-fs.readFile('./data.csv', 'utf-8', callback);
+
+try {
+    const content = await fsp.readFile('./data.csv', 'utf-8');
+    console.log(content); 
+    await parsedData(content); 
+}
+catch (error) {
+    console.log(error.message); 
+}
